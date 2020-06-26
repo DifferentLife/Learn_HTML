17b5ee9ceac46e4fcea2021bd6f8da415da953ce
DifferentLife <differentlife0725@gmail.com>
2020-06-26 19:55:37 +0700

    fiftteen

--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -3,7 +3,12 @@
 export default () => {
   return (
     <div>
-      <p className='title'>Hello</p>
+      <h1 className='title'>Hello</h1>
+      <h1 className='title title1'>World</h1>
+      <div className='center'>
+        <p id='first'>Differentlife</p>
+        <p id='first'>Web Application</p>
+      </div>
     </div>
   )
 }
