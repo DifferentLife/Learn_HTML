e92de5bcac1869be49521840c91478991e6edaa3
DifferentLife <differentlife0725@gmail.com>
2020-07-02 21:11:31 +0700

    twentyone

---
+++ nextjs/pages/foo.js
@@ -0,0 +1,9 @@
+// import styles from './styles.module.css'
+export default () => {
+    return (
+        <>
+            <h1 className='title'>Hello</h1>
+            <h1 className='title title1'>World</h1>
+        </>
+    )
+}
