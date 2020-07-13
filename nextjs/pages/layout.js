1f3f770b6951c706ba1d4218f010dd3a06470eb6
DifferentLife <differentlife0725@gmail.com>
2020-07-13 23:26:41 +0700

    thirty

--- nextjs/pages/layout.js
+++ nextjs/pages/layout.js
@@ -1,69 +1,165 @@
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu } from 'antd';
 import {
-    DesktopOutlined,
-    PieChartOutlined,
-    FileOutlined,
+    AppstoreOutlined,
+    BarChartOutlined,
+    CloudOutlined,
+    ShopOutlined,
     TeamOutlined,
     UserOutlined,
+    UploadOutlined,
+    VideoCameraOutlined,
 } from '@ant-design/icons';
 
 const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
-
-
-const Foo = () => {
-    class SiderDemo extends React.Component {
-        state = {
-            collapsed: false,
-        }
-    }
-
-    onCollapse = collapsed => {
-        console.log(collapsed);
-        this.setState({ collapsed });
-    };
-    return (
-        <div>
-            <Layout>
-                <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-                    <div className="logo" />
-                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                        <Menu.Item key="1" icon={<UserOutlined />}>
-                            nav 1
-            </Menu.Item>
-                        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                            nav 2
-            </Menu.Item>
-                        <Menu.Item key="3" icon={<UploadOutlined />}>
-                            nav 3
-            </Menu.Item>
-                    </Menu>
-                </Sider>
-                <Layout className="site-layout">
-                    <Header className="site-layout-background" style={{ padding: 0 }}>
-                        {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                            className: 'trigger',
-                            onClick: this.toggle,
-                        })}
-                    </Header>
-                    <Content
-                        className="site-layout-background"
-                        style={{
-                            margin: '24px 16px',
-                            padding: 24,
-                            minHeight: 280,
-                        }}
-                    >
-                        Content
-          </Content>
-                </Layout>
+return (
+    <div>
+        <Layout>
+            <Sider
+                style={{
+                    overflow: 'auto',
+                    height: '100vh',
+                    position: 'fixed',
+                    left: 0,
+                }}
+            >
+                <div className="logo" />
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+                    <Menu.Item key="1" icon={<UserOutlined />}>
+                        nav 1
+        </Menu.Item>
+                    <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                        nav 2
+        </Menu.Item>
+                    <Menu.Item key="3" icon={<UploadOutlined />}>
+                        nav 3
+        </Menu.Item>
+                    <Menu.Item key="4" icon={<BarChartOutlined />}>
+                        nav 4
+        </Menu.Item>
+                    <Menu.Item key="5" icon={<CloudOutlined />}>
+                        nav 5
+        </Menu.Item>
+                    <Menu.Item key="6" icon={<AppstoreOutlined />}>
+                        nav 6
+        </Menu.Item>
+                    <Menu.Item key="7" icon={<TeamOutlined />}>
+                        nav 7
+        </Menu.Item>
+                    <Menu.Item key="8" icon={<ShopOutlined />}>
+                        nav 8
+        </Menu.Item>
+                </Menu>
+            </Sider>
+            <Layout className="site-layout" style={{ marginLeft: 200 }}>
+                <Header className="site-layout-background" style={{ padding: 0 }} />
+                <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                    <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
+                        ...
+          <br />
+          Really
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          long
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          ...
+          <br />
+          content
+        </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
             </Layout>
+        </Layout>,
     );
   }
 }
 
-        </div >
-    )
+    </div >
+)
 }
 
 export default Foo
