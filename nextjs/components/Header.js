0d203af42ca49f24bab5b5ca2f8a87f5061967c1
DifferentLife <differentlife0725@gmail.com>
2020-07-19 23:55:36 +0700

    twenty-five

--- nextjs/components/Header.js
+++ nextjs/components/Header.js
@@ -1,9 +1,12 @@
 import Link from 'next/link'
+import { Layout, Menu, Breadcrumb } from 'antd';
 
-const Header = () => {
+const { Header, Content, Footer } = Layout;
+
+const Headers = () => {
     return (
         <div>
-            <div className='center flexbox-container'>
+            {/* <div className='center flexbox-container'>
                 <div>
                     <Link href="/">
                         <a>Home</a>
@@ -24,10 +27,45 @@ const Header = () => {
                         </Link>
                     </div>
                 </span>
-            </div>
+            </div> */}
+            <Layout className="layout">
+                <Header>
+                    <div className="logo" />
+                    <Menu theme="dark" mode="horizontal">
+                        <Menu.Item key="1">
+                            <Link href="/">
+                                <a>Home</a>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Link href="/information">
+                                <a>Information</a>
+                            </Link>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <a href="https://www.facebook.com/teerapol.yingsua" target="blank">Facebook</a>
+                        </Menu.Item>
+                        <Menu.Item key="4">
+                            <Link href="/Login">
+                                <a>Login</a>
+                            </Link>
+                        </Menu.Item>
+                    </Menu>
+                </Header>
+                {/* <Content style={{ padding: '0 50px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>Home</Breadcrumb.Item>
+                        <Breadcrumb.Item>List</Breadcrumb.Item>
+                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div className="site-layout-content">Content</div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer> */}
+            </Layout>
+
 
         </div>
     )
 }
 
-export default Header
+export default Headers
--- nextjs/pages/layout.js
+++ nextjs/pages/layout.js
@@ -1,17 +1,24 @@
 import { Carousel } from 'antd';
 
-ReactDOM.render(
-  <Carousel autoplay>
-    <div>
-      <h3>1</h3>
-    </div>
-    <div>
-      <h3>2</h3>
-    </div>
-    <div>
-      <h3>3</h3>
-    </div>
-    <div>
-      <h3>4</h3>
-    </div>
-  </Carousel>
+
+const Layout = () => {
+  return (
+   <div>
+      <Carousel autoplay>
+      <div>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+    </Carousel>
+   </div>
+  )
+}
+ export default Layout
